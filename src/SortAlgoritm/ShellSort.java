8e5114a7d4b4c142745bebd40b6bbbcc70bdbf7e
jiemu <rebianne03@naver.com>
2023-07-27 22:16:52 +0900

    2023.07.26 algoritm-java - 쉘정렬 2 : 범위설정 변경

--- src/SortAlgoritm/ShellSort.java
+++ src/SortAlgoritm/ShellSort.java
@@ -14,4 +14,22 @@ public class ShellSort {
             }
         }
     }
+
+    // h값을 반으로 나누기만 하면 그룹끼리 정렬되지 않음 :: 서로 배수가 되지 않도록 해야함
+    static void shellSort2(int[] a, int n) {
+        int h;
+        for (h = 1; h < n; h= h * 3 + 1) // 1부터 시작해서 3배 곱하고 1더한 값 < n인 범위까지
+            ;
+
+        for ( ; h > 0; h /= 3) {
+            for (int i = h; i < n; i++) {
+                int j;
+                int tmp = a[i];
+                for (j = i - h; j >= 0 && a[j] > tmp; j -= h) {
+                    a[j + h] = a[j];
+                }
+                a[j + h] = tmp;
+            }
+        }
+    }
 }
