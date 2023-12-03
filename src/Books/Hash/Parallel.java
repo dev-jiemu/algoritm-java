6dc6a7d550968a07e834589d70200783a439224d
jiemu <rebianne03@naver.com>
2023-12-03 13:06:46 +0900

    2023.11.25 algoritm-java - 프로그래머스 코딩 테스트 문제 풀이 전략 - Hash : 평행 12번 케이스부터 통과 못하던 것 수정

--- src/Books/Hash/Parallel.java
+++ src/Books/Hash/Parallel.java
@@ -1,27 +1,37 @@
 package src.Books.Hash;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/120875
-// 테스트 케이스 12번부터 미스나는데 수정 필요함
+// Ref. https://school.programmers.co.kr/questions/58575
 public class Parallel {
 
     // 모든 점 사이의 기울기를 계산
     public int solution(int[][] dots) {
-        Set<Double> slopes = new HashSet<>();
-        for(int i = 0; i < dots.length; i++) {
+        Map<Double, Set<Integer>> slopeMap = new HashMap<>();
+
+        for (int i = 0; i < dots.length; i++) {
             for (int j = i + 1; j < dots.length; j++) {
                 double slope = getSlope(dots[i][0], dots[i][1], dots[j][0], dots[j][1]);
-                // 같은 기울기가 있으면 평행한 선이 존재하는 것임
-                if (slopes.contains(slope)) {
-                    return 1;
-                }
-                slopes.add(slope);
+
+                slopeMap.computeIfAbsent(slope, k -> new HashSet<>()).add(i);
+                slopeMap.computeIfAbsent(slope, k -> new HashSet<>()).add(j);
             }
         }
 
-        return 0;
+        int result = 0;
+
+        for (Set<Integer> indices : slopeMap.values()) {
+            if (indices.size() >= 4) {
+                result = 1;
+                break;
+            }
+        }
+
+        return result;
     }
 
     public double getSlope(int x1, int y1, int x2, int y2) {
