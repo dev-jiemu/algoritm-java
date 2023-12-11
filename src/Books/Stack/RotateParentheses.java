34f1be956da32b46bcce28b3ce571ca16416b5b6
rebia <rebianne03@naver.com>
2023-12-11 23:26:02 +0900

    2023.12.11 algoritm-java - 프로그래머스 코딩 테스트 문제 풀이 전략 - 스택 : 괄호 회전하기

---
+++ src/Books/Stack/RotateParentheses.java
@@ -0,0 +1,46 @@
+package src.Books.Stack;
+
+import java.util.*;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/76502
+public class RotateParentheses {
+    public int solution(String s) {
+        char[] str = s.toCharArray();
+        int count = 0;
+
+        for (int offset = 0; offset < str.length; offset++) {
+            if(isCorrect(str, offset)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // 짝 맞는지 체크해야함
+    private boolean isCorrect(char[] str, int offset) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length; i++) {
+            char c = str[(offset + i) % str.length];  // substring 으로 구현할수 있긴 한데 이건 매번 쓸때마다 문자열을 새로 만듬
+            switch (c) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[' :
+                    stack.push(']');
+                    break;
+                case ')' :
+                case '}' :
+                case ']' :
+                    if (stack.isEmpty()) return false;
+                    if (stack.pop() != c) return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
