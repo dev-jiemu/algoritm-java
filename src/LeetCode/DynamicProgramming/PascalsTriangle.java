0622e9c56614db0659a1e3ea65f4358397851ba3
rebia <rebianne03@naver.com>
2025-06-17 23:58:54 +0900

    2025.06.17 LeetCode 코딩테스트 연습 - Pascal`s Triangle (DP)

--- src/LeetCode/DynamicProgramming/PascalsTriangle.java
+++ src/LeetCode/DynamicProgramming/PascalsTriangle.java
@@ -13,20 +13,28 @@ public class PascalsTriangle {
         first.add(1);
         result.add(first);
 
+        if (numRows == 1) {
+            return result;
+        }
+
         // index 2
         List<Integer> second = new ArrayList<>();
         second.add(1);
         second.add(1);
         result.add(second);
 
+        if (numRows == 2) {
+            return result;
+        }
+
         // index 3 부터 DP
         if (numRows > 2) {
-            for(int i = 3; i <= numRows; i++) {
+            for(int i = 2; i < numRows; i++) {
                 List<Integer> prev = result.get(i - 1);
                 List<Integer> next = new ArrayList<>();
 
                 next.add(1);
-                for(int j = 1; j < i - 1; j++)  {
+                for(int j = 1; j <= i - 1; j++)  {
                     next.add(prev.get(j - 1) + prev.get(j));
                 }
                 next.add(1);
