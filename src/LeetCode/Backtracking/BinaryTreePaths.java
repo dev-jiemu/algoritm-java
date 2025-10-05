447b02dfb55b2645e526c39acd8546dbe13093e0
jiemu <rebianne03@naver.com>
2025-10-05 21:41:28 +0900

    2025.10.05 LeetCode 코딩테스트 연습 - BinaryTreePaths (Backtracking)

--- src/LeetCode/Backtracking/BinaryTreePaths.java
+++ src/LeetCode/Backtracking/BinaryTreePaths.java
@@ -3,38 +3,34 @@ package src.LeetCode.Backtracking;
 import src.LeetCode.common.TreeNode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 // https://leetcode.com/problems/binary-tree-paths/description/?envType=problem-list-v2&envId=backtracking
 public class BinaryTreePaths {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new LinkedList<>();
-        if (root == null) return result;
-
-        List<Integer> path = new ArrayList<>();
-        dfsWithList(root, path, result);
-
+        List<String> result = new ArrayList<>();
+        if (root != null) {
+            dfs(root, "", result);
+        }
         return result;
     }
 
-    private void dfsWithList(TreeNode node, List<Integer> path, List<String> result) {
-        if (node == null) return;
+    private void dfs(TreeNode node, String path, List<String> result) {
+        path += node.val;
 
-        path.add(node.val);
-
+        // Leaf node
         if (node.left == null && node.right == null) {
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < path.size(); i++) {
-                if (i > 0) sb.append("->");
-                sb.append(path.get(i));
-            }
-            result.add(sb.toString());
-        } else {
-            dfsWithList(node.left, path, result);
-            dfsWithList(node.right, path, result);
+            result.add(path);
+            return;
         }
 
-        path.remove(path.size() - 1);
+        // 자식 노드들 탐색
+        path += "->";
+        if (node.left != null) {
+            dfs(node.left, path, result);
+        }
+        if (node.right != null) {
+            dfs(node.right, path, result);
+        }
     }
 }
