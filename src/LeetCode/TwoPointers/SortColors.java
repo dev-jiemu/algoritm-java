1c1cd53443f2fee8719203853d0c90ac304646b9
rebia <rebianne03@naver.com>
2025-12-16 21:11:33 +0900

    2025.12.16 LeetCode 코딩테스트 연습 - SortColors (Two Pointer)

---
+++ src/LeetCode/TwoPointers/SortColors.java
@@ -0,0 +1,30 @@
+package src.LeetCode.TwoPointers;
+
+public class SortColors {
+    // one-pass & in-place
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int current = 0;
+
+        while (current <= right) {
+            if (nums[current] == 0) {
+                swap(nums, left, current);
+                left++;
+                current++;
+            } else if (nums[current] == 2){
+                swap(nums, current, right);
+                right--;
+            } else {
+                current++;
+            }
+        }
+
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
