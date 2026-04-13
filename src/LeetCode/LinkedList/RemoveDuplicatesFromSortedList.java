a8ebe3be00335ae3cce9aaf102b7c5d627e779f7
rebia <rebianne03@naver.com>
2026-04-13 22:16:16 +0900

    2026.04.13 LeetCode 코딩테스트 연습 - RemoveDuplicatesFromSortedList (Linked List)

---
+++ src/LeetCode/LinkedList/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,25 @@
+package src.LeetCode.LinkedList;
+
+import src.LeetCode.common.ListNode;
+
+// 83. Remove Duplicates from Sorted List
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
