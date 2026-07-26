4e80bc3e4db20e1249e29d717edfc05f29c9e2dd
jiemu <rebianne03@naver.com>
2026-07-26 21:52:11 +0900

    2026.07.26 LeetCode 코딩테스트 연습 - KRadiusSubarrayAverages (Prefix Sum)

---
+++ src/LeetCode/PrefixSum/KRadiusSubarrayAverages.java
@@ -0,0 +1,25 @@
+package src.LeetCode.PrefixSum;
+
+import java.util.Arrays;
+
+// 2090. K Radius Subarray Averages
+// https://leetcode.com/problems/k-radius-subarray-averages/description/
+public class KRadiusSubarrayAverages {
+    public int[] getAverages(int[] nums, int k) {
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+
+        int[] output = new int[nums.length];
+        Arrays.fill(output, -1);
+
+        for (int i = k; i < nums.length - k; i++) {
+            int sum = (i - k - 1 >= 0) ? prefix[i+k] - prefix[i-k-1] : prefix[i+k];
+            output[i] = sum / (2 * k + 1);
+        }
+
+        return output;
+    }
+}
